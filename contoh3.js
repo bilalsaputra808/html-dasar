48312381f1e56de48cc974d7d9f2488c26c57b95
bilalsaputra808 <bilalsaputra808@gmail.com>
2022-04-02 13:57:39 +0700

    update card

---
+++ contoh3.js
@@ -0,0 +1,11 @@
+// contoh 3
+let a = 1;
+let b = 0;
+
+switch (a) {
+  case b + 1:
+    console.log("this runs, because a is 1, exactly equals b + 1");
+    break;
+  default:
+    console.log("this doesn't run");
+}
